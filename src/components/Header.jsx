10572d5e9b02772fc2505eafd0d3f981a273488b
Melissa <chopinetmelissa@live.fr>
2023-05-30 19:30:50 +0200

    Header fait

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,14 @@
-import imgK from "../assets/VectorK.png"
-import img from "../assets/Vector.png"
-import imgS from "../assets/VectorS.png"
-import imgA from "../assets/VectorA.png"
+import logoHeader from "../assets/header_logo.png"
+
 import "../styles/Header.scss"
  
 function Header() {
     return (
         <div className="barre">
-            <div className="LogoKasa">
-            <img src={imgK} alt="K" className="K"/>
-            <img src={img} alt="Maison" className="autreLettre"/>
-            <img src={imgS} alt="S" className="autreLettre"/>
-            <img src={imgA} alt="A" className="autreLettre"/>
-            </div>
+            
+            <img src={logoHeader} alt="logo" className="logo"/>
+       
+        
         <nav className="liens">
             <a href="#" className="accueil">Accueil</a>
             <a href="#" className="about">A propos</a>
