c924e3c2bd7c066655d46c5e2c3e2bf9e366029a
Melissa <chopinetmelissa@live.fr>
2023-05-30 19:49:23 +0200

    Footer Fait

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 //import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header";
-//import Footer from "./components/Footer";
+//import Header from "./components/Header";
 //import Error404 from "./pages/Error404";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/*<Header />*/}
       {/*<Error404 />*/}
-      {/*}  <Footer />*/}
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,10 @@
+import logoFooter from "../assets/footer_logo.png"
+import "../styles/Footer.scss"
+
 function Footer() {
-  return <div>
-    <h1>Hello you</h1>;
+  return <div className="footer">
+    <img src={logoFooter} alt="Logo Kasa" className="logo" />
+    <p className="text">© 2020 Kasa. All rights reserved</p>;
     </div>
 }
 
