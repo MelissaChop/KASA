124d03aaf350e04eb76ecbba0f63dd280451ba5d
Melissa <chopinetmelissa@live.fr>
2023-05-31 15:49:17 +0200

    Creation Route About Vide

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Error404 from "./pages/Error404";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/*" element={<Error404 />} />
+        <Route path="/About" element={<About />} />
       </Routes>
       <Footer />
     </>
---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import "../styles/About.css"
+
+function About (){
+    return(
+        <div>
+            <h1>About VIDE</h1>
+        </div>
+    )
+}
+
+export default About
