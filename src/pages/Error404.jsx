720a7ca6905b23671797e85e2a600e3d8a1eb23a
Melissa <chopinetmelissa@live.fr>
2023-05-31 12:09:19 +0200

    Ajout title

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,11 +1,13 @@
 import "../styles/Error404.scss"
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Error404(){
     return <div className="pageError">
       
         <p className="Err404">404</p>
         <p className="textError">Oups! La page que vous demandez n'existe pas!</p>
-       <a href = "#" className="lien">Retourner sur la page d'accueil</a>
+       <Link to="/" className="lien">Retourner sur la page d'accueil</Link>
         
     </div>
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import Banner from "../assets/banner.png"
 import "../styles/Home.scss"
 
-function Home(){
-return <div className="homePage">
-    <img src={Banner} alt="Rochets" className="imgR"/>
-    <p className="texteInImage">Chez vous, partout et ailleurs</p>
-    
-</div>
+function Home() {
+ 
+
+  return (
+    <div className="homePage">
+      <img src={Banner} alt="Rochets" className="imgR" />
+      <p className="texteInImage">Chez vous, partout et ailleurs</p>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
