77bed417bb4cc517915e0aa8af27bc3b68e37072
Melissa <chopinetmelissa@live.fr>
2023-05-31 15:19:59 +0200

    Ajout route page Error404

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Error404" element={<Error404 />} />
+        <Route path="/*" element={<Error404 />} />
       </Routes>
       <Footer />
     </>
