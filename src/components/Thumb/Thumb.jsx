d68aa72c26402a377664230647e5023102622c48
Melissa <chopinetmelissa@live.fr>
2023-06-02 00:07:46 +0200

    Fin journée

--- src/components/Thumb/Thumb.jsx
+++ src/components/Thumb/Thumb.jsx
@@ -1,5 +1,5 @@
 //import style from "./Thumb.module.css"
-import React, { useState } from "react";
+import React from "react";
 
 function Thumb(props) {
   return (
--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,13 +1,15 @@
-import "./Error404.module.css";
+import style from "./Error404.module.css";
 import React from "react";
 import { Link } from "react-router-dom";
 
 function Error404() {
   return (
-    <div className="pageError">
-      <p className="Err404">404</p>
-      <p className="textError">Oups! La page que vous demandez n'existe pas!</p>
-      <Link to="/" className="lien">
+    <div className={style.pageError}>
+      <p className={style.Err404}>404</p>
+      <p className={style.textError}>
+        Oups! La page que vous demandez n'existe pas!
+      </p>
+      <Link to="/" className={style.lien}>
         Retourner sur la page d'accueil
       </Link>
     </div>
